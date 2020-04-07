1e39f4c9dbadccd269f5283386a8f5252e7912b6
Charlie Payne <charliepayne1101@gmail.com>
2020-04-07 14:19:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 // write your code below!
+var name = 'Charlie';
+var height = 75
 
-var name = "Janet"
-var height = 74
-
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height + 1)
+console.log('Name:');
+console.log(name);
+ 
+console.log('Height:');
+console.log(height + 1);
